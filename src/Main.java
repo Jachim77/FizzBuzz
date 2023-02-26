38c687e9fc4a4fe3cfa50beb23037db1089d8ac2
Jan Michal <jan.michal@email.cz>
2023-02-26 20:20:30 +0100

    Update

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        for (int i = 1; i <= 100; i++) {
+            double a1 = (double) i / 3;    // i děleno 3mi a na double
+            double a2 = (double) i / 5;    // i děleno 5ti a na double
+            int b1 = (int) a1;             // převedeno na int
+            int b2 = (int) a2;
+            double c1 = b1;              // int na double
+            double c2 = b2;
+
+
+            if ((a1 == c1) && (a2 == c2)) {                            // porovnání, jestli pův. číslo bylo celé nebo ne
+                System.out.println("FizzBuzz");
+            } else if (a1 == c1) {
+                System.out.println("Fizz");
+            } else if (a2 == b2) {
+                System.out.println("Buzz");
+            } else System.out.println(i);
+
+        }
+
+
     }
 }
